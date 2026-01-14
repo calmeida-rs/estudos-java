5008cb0987d839316974daf971ebe991d7b5eabf
Carlos Almeida <eduardoazevedo2e@gmail.com>
2026-01-13 21:57:02 -0300

    Testes métodos estáticos

--- orientacaoobjetos/application/MetodosEstaticos.java
+++ orientacaoobjetos/application/MetodosEstaticos.java
@@ -1,31 +1,21 @@
 package curso.nelioalves.orientacaoobjetos.application;
 
+import curso.nelioalves.orientacaoobjetos.entities.Calculator;
+
 import java.util.Scanner;
 
 public class MetodosEstaticos {
     //padrao de constantes é tudo maiusculo
-    public static final double PI = 3.14159;
-
-
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter radius:");
         double radius = sc.nextDouble();
-
-        double c = circunference(radius);
-
-        double v = volume(radius);
-
+        //caso a calculadora seja estática, não tem necessidade, caso eu tivesse vários cálculos seria diferente
+        double c = Calculator.circunference(radius);
+        double v = Calculator.volume(radius);
         System.out.printf("Circunference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI VALUE: %.2f", PI);
+        System.out.printf("PI VALUE: %.2f", Calculator.PI);
 
-    }
-
-    public static double circunference(double radius){
-        return 2*PI* radius;
-    }
-    public static double volume(double radius){
-        return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
