270bd13097bb89dbb1e34f4da21c9e014129fe1b
Carlos Almeida <eduardoazevedo2e@gmail.com>
2026-01-17 11:57:45 -0300

    Adicionando métodos constrututores + encapsulamento usando javabeans

--- orientacaoobjetos/application/Estoque.java
+++ orientacaoobjetos/application/Estoque.java
@@ -13,19 +13,22 @@ public class Estoque {
         String name = sc.nextLine();
         System.out.println("Price");
         double price = sc.nextDouble();
-        System.out.println("Quantity");
-        int quantity = sc.nextInt();
         /*
         Já entra recebendo os atributos, porém não pode ser inicializado antes.
          */
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
+        product.setName("Computer");
         product.showProducts();
+
         System.out.println("Entre com o número de produtos para ser adicionado no estoque: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
         product.addProducts(quantity);
         product.showProducts();
+
         System.out.println("Entre com o número de produtos para serem excluidos do estoque: ");
         int numeroExcluir = sc.nextInt();
+
         product.removeProducts(numeroExcluir);
         product.showProducts();
         sc.close();
--- orientacaoobjetos/entities/Product.java
+++ orientacaoobjetos/entities/Product.java
@@ -1,10 +1,14 @@
 package curso.nelioalves.orientacaoobjetos.entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
+    //podemos deixar um construtor padrão também pra não necessitar digitar os valores na instanciação do objeto.
+    public Product(){
+
+    }
     /*
     Adicionando um metodo construtor no meu objeto, tem como objetivo fazer com que na sua instanciação, ele seja obrigado a receber valores.
      */
@@ -12,6 +16,33 @@ public class Product {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+        //nao tem necessidade de iniciar o valor, já que o java começa com 0
+    }
+
+    //criando as regras de encapsulamento
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getQuantity(){
+        return quantity;
     }
 
     public double totalValeInStock(){
@@ -29,6 +60,7 @@ public class Product {
         System.out.println("Quantidade: " + quantity);
         System.out.printf("Total: %.2f%n", totalValeInStock());
     }
+
 
 
 }
